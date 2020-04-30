e8db31eb01b453ebf2ae53e3b487c45ddaa71bc5
jual ahmed <md.jual.ah@gmail.com>
2020-04-30 13:48:49 +0600

    make one button component

--- dev/App.vue
+++ dev/App.vue
@@ -1,12 +1,22 @@
 <template>
 <div>
-	
+  	<button class="bg-gray-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Secondary</button>
+    <button class="bg-red-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Danger</button>
+    <button class="bg-orange-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Warning</button>
+    <button class="bg-yellow-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Info</button>
+    <button class="bg-green-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Primary</button>
+    <button class="bg-green-900 hover:bg-green-800 shadow-xl px-5 py-2 rounded text-white">Dark</button>
+    <button class="bg-teal-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Inverse</button>
+    <button class="bg-blue-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Success</button>
+    <button class="bg-indigo-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Light</button>
+    <button class="bg-purple-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Purple</button>
+    <button class="bg-pink-500 hover:bg-green-500 shadow-xl px-5 py-2 rounded text-white">Pink</button>
 
-	<t-button>Primary</t-button>
-	<t-button>Primary</t-button>
-	<t-button>Primary</t-button>
-	<t-button>Primary</t-button>
-	<t-button>Primary</t-button>
+	<t-button type="primary">Primary</t-button>
+	<t-button type="primary">Primary</t-button>
+	<t-button type="primary">Primary</t-button>
+	<t-button type="primary">Primary</t-button>
+	<t-button type="primary">Primary</t-button>
 </div>
 </template>
 
--- src/components/button/TButton.vue
+++ src/components/button/TButton.vue
@@ -1,13 +1,49 @@
 <template>
-	<p class="primary">aaaaaaaaaaaaaa</p>
+	<button :type="nativeType" :disabled="loading" :class="{
+			[`${type}`]: true,
+			'btn-block': block,
+			'btn-lg': large,
+			'btn-loading': loading,
+			'opacity-50': loading
+		}">
+		<slot />
+	</button>
 </template>
 
 <script>
 export default {
 	name: 'TButton',
+	props: {
+		type: {
+			type: String,
+			default: 'primary'
+		},
+
+		nativeType: {
+			type: String,
+			default: 'submit'
+		},
+
+		loading: {
+			type: Boolean,
+			default: false
+		},
+
+		block: {
+			type: Boolean,
+			default: false
+		},
+
+		large: {
+			type: Boolean,
+			default: false
+		}
+	}
 }
 </script>
 
 <style>
-
+.primary{
+	@extend .bg-green-500;
+}
 </style>
